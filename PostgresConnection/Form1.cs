e00051b2414d90a8e0a3a0759710a17df76a7576
Konstantinos Boz <kotsos_573@hotmail.com>
2019-09-12 20:19:53 +0300

    queries for loading basic tables and custom query method add

--- PostgresConnection/Form1.cs
+++ PostgresConnection/Form1.cs
@@ -16,6 +16,8 @@ namespace PostgresConnection
     {
         private DataSet dataSet = new DataSet();
         private DataTable dataTable = new DataTable();
+        // PostgeSQL-style connection string
+        string connstring = "Server=localhost;Port=5432;Database=test;User ID=postgres;Password=passw0rd;";
         public Form1()
         {
             InitializeComponent();
@@ -25,20 +27,34 @@ namespace PostgresConnection
         {
             try
             {
-                // PostgeSQL-style connection string
-                string connstring = "Server=localhost;Port=5432;Database=test;User ID=postgres;Password=passw0rd;";
-
                 // Making connection with Npgsql provider
                 NpgsqlConnection conn = new NpgsqlConnection(connstring);
-                FileInfo file = new FileInfo("Database/person.sql");
-                //FileInfo file2 = new FileInfo("Database/Person.sql");
+
+                FileInfo file1 = new FileInfo("Database/profession.sql");
+                FileInfo file2 = new FileInfo("Database/person.sql");
+                FileInfo file3 = new FileInfo("Database/genre.sql");
+                FileInfo file4 = new FileInfo("Database/movies.sql");
+                FileInfo file5 = new FileInfo("Database/tvSeries.sql");
 
                 conn.Open();
-                // quite complex sql statement
-                string sql = file.OpenText().ReadToEnd();
-                // data adapter making request from our connection
-                NpgsqlCommand insert = new NpgsqlCommand(sql, conn);
-                insert.ExecuteNonQuery();
+                //string sqlJobs = file1.OpenText().ReadToEnd();
+                //string sqlPerson = file2.OpenText().ReadToEnd();
+                //string sqlGenre = file3.OpenText().ReadToEnd();
+                //string sqlMovies = file4.OpenText().ReadToEnd();
+                //string sqlTv = file5.OpenText().ReadToEnd();
+                ////// data adapter making request from our connection
+                //NpgsqlCommand insertJobs = new NpgsqlCommand(sqlJobs, conn);
+                //insertJobs.ExecuteNonQuery();
+                //NpgsqlCommand insertPerson = new NpgsqlCommand(sqlPerson, conn);
+                //insertPerson.ExecuteNonQuery();
+                //NpgsqlCommand insertGenre = new NpgsqlCommand(sqlGenre, conn);
+                //insertGenre.ExecuteNonQuery();
+                //NpgsqlCommand insertMovies = new NpgsqlCommand(sqlMovies, conn);
+                //insertMovies.ExecuteNonQuery();
+                //NpgsqlCommand insertTv = new NpgsqlCommand(sqlTv, conn);
+                //insertTv.ExecuteNonQuery();
+                string addCrew = "insert into movieAssignment (assignmentId, personId, professionId, movieId) values (1, 5, 4, 1)";
+                ExcecuteInsertQuery(addCrew);
                 //NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
                 //// i always reset DataSet before i do
                 //// something with it.... i don't know why :-)
@@ -49,15 +65,23 @@ namespace PostgresConnection
                 //dataTable = dataSet.Tables[0];
                 ////// connect grid to DataTable
                 //dataGridView1.DataSource = dataTable;
-                // since we only showing the result we don't need connection anymore
                 conn.Close();
             }
             catch (Exception msg)
             {
-                // something went wrong, and you wanna know why
                 MessageBox.Show(msg.ToString());
                 throw;
             }
+        }
+        private void ExcecuteInsertQuery(string sql)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(connstring);
+            conn.Open();
+
+            NpgsqlCommand newQuery = new NpgsqlCommand(sql, conn);
+            newQuery.ExecuteNonQuery();
+
+            conn.Close();
         }
     }
 }
