293a15fc5a294b0e640df0c74b9d66598f3a85ca
Konstantinos Boz <kotsos_573@hotmail.com>
2019-09-18 14:09:11 +0300

    dafds

--- PostgresConnection/CrewAssignmentForm.Designer.cs
+++ PostgresConnection/CrewAssignmentForm.Designer.cs
@@ -34,7 +34,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -109,22 +108,11 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(1096, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "Έξοδος";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // CrewAssignmentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1183, 664);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -150,6 +138,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
--- PostgresConnection/CrewAssignmentForm.cs
+++ PostgresConnection/CrewAssignmentForm.cs
@@ -111,10 +111,5 @@ namespace PostgresConnection
                 }
             }
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
     }
 }
